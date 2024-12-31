4af78067759dc0c62fec687a4c000192df6ed502
Otu Udo <119851008+Otumiky@users.noreply.github.com>
2024-12-31 21:02:07 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket        = var.bucket_name
+  acl           = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
