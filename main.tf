315a35360e75a999319389eca1cbc2f8a901298b
Otu Udo <119851008+Otumiky@users.noreply.github.com>
2024-12-31 21:03:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,28 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket        = var.bucket_name
+  acl           = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
